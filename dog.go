baad5d4a7bcbe2df99a129ce53e46d4a5078b295
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-07-08 22:00:34 +0530

    when child

--- dog.go
+++ dog.go
@@ -7,3 +7,7 @@ import (
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
+
+func Whenchild(s string) string {
+	return "When the puppy is child it says: " + strings.ToLower(s)
+}
